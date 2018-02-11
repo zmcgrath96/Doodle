0cbcac8011303246144adac09fd0ebd0d539e365
Nick Alvarez <n166a067@ku.edu>
2018-02-10 21:02:21 -0600

    Fixed core dump issue

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -8,15 +8,23 @@ int main(int argc, char * argv[])
 	User A("Nick",true);
 	cout<<endl;
 	User B("Ryan",false);
+	cout<<endl;
+	User C("Jack",true);
 	A.AddTime(1);
 	A.AddTime(2);
+
 	B.AddTime(3);
 	B.AddTime(4);
+
+	C.AddTime(5);
+	C.AddTime(6);
+
 	E.addUser(A);
 	E.addUser(B);
+	E.addUser(C);
 	cout<<"Event name: "<<E.getName()<<endl;
 	cout<<"Event date: "<<E.getDate()<<endl;
 	E.getAvailableTimes();
-	//E.getUsers();
+	E.getUsers();
 	return 0;
 }
