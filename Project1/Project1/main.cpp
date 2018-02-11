5836d38084d02add09de7c3dae7d6f2f0f1280f1
Nick Alvarez <n166a067@ku.edu>
2018-02-11 01:22:59 -0600

    Fixed merge conflicts

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char * argv[])
 	Event E;
 	E.setEventName("B-day");
 	E.setEventDate("Feb.2");
-	cout<<"1"<<endl;
 	User A;
 	A.setName("Nick");
 	A.setisAdmin(true);
@@ -19,7 +18,6 @@ int main(int argc, char * argv[])
 	User C;
 	C.setName("Jack");
 	C.setisAdmin(true);
-	cout<<"2"<<endl;
 	A.AddTime(1);
 	A.AddTime(2);
 
@@ -28,14 +26,11 @@ int main(int argc, char * argv[])
 
 	C.AddTime(5);
 	C.AddTime(6);
-	cout<<"3"<<endl;
 
 	E.addUser(A);
 	E.addUser(B);
 	E.addUser(C);
-	cout<<"4"<<endl;
 	Exec.AddEvent(E);
-	cout<<"Made it past add event"<<endl;
 	Exec.getAllEvents(); //prints all event names
 	cout<<"-------------------------------"<<endl;
 	Exec.checkAval(); //prints all times under event names 
