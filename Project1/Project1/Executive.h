8a906ad8440fe03a73d2688fd98eca2066627c08
Andrew Hubbard <andrew.hubbard@ku.edu>
2018-02-12 08:50:35 -0600

    Small Documentation Changes
    Added an extra star to the front of the comments in the .h files so that Doxygen recognizes the comments as documentation for the methods.

--- Project1/Project1/Executive.h
+++ Project1/Project1/Executive.h
@@ -10,56 +10,56 @@
 class Executive
 {
 	public:
-		/*
+		/**
 		*	Creates the object
 		*	@param None
 		*	@return None
 		*/
 		Executive();
 
-		/*
+		/**
 		*	Does nothing
 		*	@param None
 		*	@return None
 		*/
 		~Executive();
 
-		/*
+		/**
 		*	Adds a given event to the vector that stores all the events
 		*	@param An event to be added to the vector
 		*	@return None
 		*/
 		void AddEvent(Event E);
 
-		/*
+		/**
 		*	Returns the name of the events that the given user is admin of
 		*	@param A string representing the name of a user
 		*	@return A vector containing the names of the events that the given user is admin of
 		*/
 		std::vector<std::string> checkAval(std::string Name);
 
-		/*
+		/**
 		*	Creates a string array and stores the names of all the events currently stored in the vector
 		*	@param None
 		*	@return An array of all the names of the events currently stored in the vector
 		*/
 		std::string* getAllEvents();
 
-		/*
+		/**
 		*	Checks the current size of the event vector and returns it
 		*	@param None
 		*	@return The size of the event vector
 		*/
 		int getEventSize();
 
-		/*
+		/**
 		*	Stores the events list, the users, and their avaliabilities in a file so that the information is saved between program executions
 		*	@param None
 		*	@return None
 		*/
 		void write();
 
-		/*
+		/**
 		*	Reads the information stored in the save file and populates the events list, the users within each event, and the avaliabilites of each user
 		*	@param None
 		*	@return None
--- Project1/Project1/User.h
+++ Project1/Project1/User.h
@@ -5,56 +5,56 @@
 class User
 {
 	public:
-		/*
+		/**
 		*	Creates the bool array m_Times and sets it to false. Creates the m_Strings array and populates it with the possible time slots.
 		*	@param None
 		*	@return None
 		*/
 		User();
 
-		/*
+		/**
 		*	Does Nothing
 		*	@param None
 		*	@return None
 		*/
 		~User();
 
-		/*
+		/**
 		*	Sets the user's name
 		*	@param String representing the name of the user
 		*	@return None
 		*/
 		void setName(std::string name);
 
-		/*
+		/**
 		*	Returns the name of the user
 		*	@param None
 		*	@return The string representing the name of the user
 		*/
 		std::string getUserName();
 
-		/*
+		/**
 		*	Returns a bool at the index passed in which represents whether the user is avaliable at that time
 		*	@param An int representing an index in the boolean array m_Times
 		*	@return The boolean stored at the given index
 		*/
 		bool getTime(int i);
 
-		/*
+		/**
 		*	Returns the string at the index passed in which represents a time slot
 		*	@param An int representing an index in the string array m_Strings
 		*	@return The string at the given index
 		*/
 		std::string getStrings(int j);
 
-		/*
+		/**
 		*	Toggles the bool at the given index
 		*	@param An index representing a bool in m_Times
 		*	@return None
 		*/
 		void AddTime(int T);
 
-		/*
+		/**
 		*	Prints the times that the user is avaliable
 		*	@param None
 		*	@return None
