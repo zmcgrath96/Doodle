2fa1d68e53ccf5e2fba30b940db30e68231274a2
Andrew Hubbard <andrew.hubbard@ku.edu>
2018-02-12 08:56:10 -0600

    Documentation Part 1
    Added HTML documentation for User, Event, and Executive.

---
+++ documentation/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['event',['Event',['../class_event.html',1,'']]],
+  ['executive',['Executive',['../class_executive.html',1,'']]]
+];
---
+++ documentation/html/search/functions_6.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['setadmin',['setAdmin',['../class_event.html#aec0fa6f68332424cdfb15855765cadf3',1,'Event']]],
+  ['seteventdate',['setEventDate',['../class_event.html#a0fbbd58ef1341dfacea38de7795d3667',1,'Event']]],
+  ['seteventname',['setEventName',['../class_event.html#a4c6829c10c6c865759221d57a0f4a64f',1,'Event']]],
+  ['setname',['setName',['../class_user.html#a93c208c3a71df99ca19a1782f6d32c34',1,'User']]],
+  ['setnumofus',['setNumOfUs',['../class_event.html#ab573b9da583f00cd5d60934fb594e1ac',1,'Event']]]
+];
