d7bd5e2c9d99b24e7a61be7e2cf1005e22708cc2
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-02-25 20:49:19 -0600

    Fixed day and event

--- Project1/Project1/Day.h
+++ Project1/Project1/Day.h
@@ -6,11 +6,27 @@
 using namespace std;
 
 class Day {
+    public: 
+    /**
+		*	Constructor
+		*	@param None
+		*	@return None
+		*/
+    Day();
+
+    /**
+		*	Constructor
+		*	@param ints
+		*	@return None
+		*/
+    Day(int eventMonth, int eventDay, int eventYear, int eventTime);
+
+
     private:
-        int month;
-        int day;
-        int year;
-        int time;  // time is in seconds from the beggining of the day.
+        int m_month; // Integer that keeps track of month
+        int m_day; // Integer that keeps track of day
+        int m_year; // Integer that keeps track of year
+        int m_time;  // time is in seconds from the beggining of the day.
 
 };
 #endif
--- Project1/Project1/Event.cpp
+++ Project1/Project1/Event.cpp
@@ -1,3 +1,14 @@
 #include <algorithm>
 #include "Event.h"
 
+using namespace std;
+
+Event::Event(){}
+
+Event::Event(Day startDay, Day endDay, string name, User admin){
+	m_startDay = startDay;
+	m_endDay = endDay;
+	m_name = name;
+	m_admin = admin;
+}
+
