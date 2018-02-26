fe53ea24b1d377f476dc2d05209b079a96d54af2
Coltyn Gatton <c527g802@ku.edu>
2018-02-26 09:12:33 -0600

    Attempting to figure stuff out

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -2862,8 +2862,16 @@ private: System::Windows::Forms::Button^  button2;
 	}
 
 	private: System::Void monthCalendar1_DateChanged(System::Object^  sender, System::Windows::Forms::DateRangeEventArgs^  e) {
-		textBox4->Text = monthCalendar1->SelectionRange->Start.ToShortDateString();
-		//System::String^ date = textBox4->Text;
+		//Display the dates for selected range
+		Label1.Text = "Dates Selected from :" + (MonthCalendar1.SelectionRange.Start() + " to " + MonthCalendar1.SelectionRange.End());
+		std::cout>>Label1.text;
+
+		//To display single selected of date
+		//MonthCalendar1.MaxSelectionCount = 1;
+
+		//To display single selected of date use MonthCalendar1.SelectionRange.Start/ MonthCalendarSelectionRange.End
+		Label2.Text = "Date Selected :" + MonthCalendar1.SelectionRange.Start;
+
 		std::string date = msclr::interop::marshal_as<std::string>(textBox4->Text);
 		//std::string newstr = gcnew String(date.c_str());
 
@@ -2889,7 +2897,7 @@ private: System::Windows::Forms::Button^  button2;
 		textBox5->Text = gcnew String(newDate.c_str());
 		if (newDate == "12/25" || newDate == "7/4" || newDate == "1/1")
 		{
-			MessageBox::Show("Select Another Date");
+			MessageBox::Show("You may not schedule an event on this date.");
 			textBox4->Clear();
 		}
 	}
