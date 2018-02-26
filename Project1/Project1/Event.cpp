7dec7d4d743a08b6fa8850ab8a4520a729045eee
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-02-26 08:59:52 -0600

    Gets for Event

--- Project1/Project1/Event.cpp
+++ Project1/Project1/Event.cpp
@@ -16,14 +16,16 @@ Event::Event(Day startDay, Day endDay, string name, User admin){
 	m_name = name;
 	m_admin = admin;
 	m_task = new Task[5];
-	numTasks = 0;
+	m_numTasks = 0;
 }
+
+
 
 bool Event::addTask(string tName){
 	Task temp = Task(tName);
 
 	// If we have hit size of array, double it
-	if (numTasks == m_task.size()){
+	if (m_numTasks >= m_task.size()){
 		Task* temp2 = new Task[m_task.size()*2];
 		for (int i = 0; i < m_task.size(); i++){
 			temp2[i] = m_task[i];
@@ -33,6 +35,39 @@ bool Event::addTask(string tName){
 	}
 	
 	m_task[numTasks] = temp;
-	numTasks++;
+	m_numTasks++;
+	return true;
 }
 
+
+
+//---------------------------------------------------
+//
+//				GETS
+//
+//---------------------------------------------------
+
+
+Day Event::getStartDay(){
+	return m_startDay;
+}
+
+Day Event::getEndDay(){
+	return m_endDay;
+}
+
+string Event::getName(){
+	return m_name;
+}
+
+User Event::getAdmin(){
+	return m_admin;
+}
+
+Task* Event::getTasks(){
+	return m_task;
+}
+
+int Event::getNumTasks(){
+	return m_numTasks;
+}
--- Project1/Project1/Event.h
+++ Project1/Project1/Event.h
@@ -43,13 +43,57 @@ class Event
 	bool addTask(string tName);
 
 
+	/**
+		*	getStartDay
+		*	@param none
+		*	@return Day object
+		*/
+	Day getStartDay();
+
+	/**
+		*	getEndDay
+		*	@param none
+		*	@return Day object
+		*/
+	Day getEndDay();
+
+
+	/**
+		*	getName
+		*	@param none
+		*	@return String
+		*/
+	string getName();
+
+	/**
+		*	getAdmin
+		*	@param none
+		*	@return User object
+		*/
+	User getAdmin();
+
+	/**
+		*	getTasks
+		*	@param none
+		*	@return Task pointer to array
+		*/
+	Task* getTasks();
+
+	/**
+		*	getNumTaks
+		*	@param none
+		*	@return Integer
+		*/
+	int getNumTasks();
+
+
 	private:
 		Day m_startDay;
 		Day m_endDay;
 		string m_name;
 		User m_admin;
 		Task* m_task;
-		int numTasks;
+		int m_numTasks;
 
 
 };
