48b0b6870df8abf218f495d2068c19964e17d74b
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-02-25 21:15:01 -0600

    Event update

--- Project1/Project1/Event.cpp
+++ Project1/Project1/Event.cpp
@@ -5,10 +5,34 @@ using namespace std;
 
 Event::Event(){}
 
+Event::~Event(){
+	delete m_task;
+}
+
+
 Event::Event(Day startDay, Day endDay, string name, User admin){
 	m_startDay = startDay;
 	m_endDay = endDay;
 	m_name = name;
 	m_admin = admin;
+	m_task = new Task[5];
+	numTasks = 0;
+}
+
+bool Event::addTask(string tName){
+	Task temp = Task(tName);
+
+	// If we have hit size of array, double it
+	if (numTasks == m_task.size()){
+		Task* temp2 = new Task[m_task.size()*2];
+		for (int i = 0; i < m_task.size(); i++){
+			temp2[i] = m_task[i];
+		}
+		delete m_task;
+		m_task = temp2;
+	}
+	
+	m_task[numTasks] = temp;
+	numTasks++;
 }
 
--- Project1/Project1/Event.h
+++ Project1/Project1/Event.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <array>
 #include "User.h"
 #include "Day.h"
 #include "Task.h"
@@ -19,6 +20,14 @@ class Event
 		*/
 	Event();
 
+
+	/**
+	*	Destroys the Event
+	*	@param
+	*	@return
+	*/
+	~Event();
+
 	/**
 		*	Constructor
 		*	@param Day object, Day object, string, User object
@@ -26,12 +35,21 @@ class Event
 		*/
 	Event(Day startDay, Day endDay, string name, User admin);
 
+	/**
+		*	addTask
+		*	@param string name
+		*	@return true
+		*/
+	bool addTask(string tName);
+
 
 	private:
 		Day m_startDay;
 		Day m_endDay;
 		string m_name;
 		User m_admin;
+		Task* m_task;
+		int numTasks;
 
 
 };
